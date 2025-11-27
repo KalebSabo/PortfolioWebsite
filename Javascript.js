d15df0abfb28b34d9fa06950318bce6ad5ee6efb
Sabo <kaleb.sabo.25@gmail.com>
2025-11-27 14:15:55 +0000

    Added more Javascript learning shenanigans, small readme edit, edit javascript file with comments

--- Javascript.js
+++ Javascript.js
@@ -1,6 +1,13 @@
 function myFunction() {
-				document.getElementById("heading").innerHTML = "Pressed Button";
-				window.alert("Button Pressed!");
+				/* 
+                accesses the document object's getElementById function, passes in the parameter "heading", 
+                then accesses the innerHTML property of the returned result (from the function)
+                 and assigns the string "Pressed Button" to the property
+                */
+                document.getElementById("heading").innerHTML = "Pressed Button"; 
+
+                // Accesses the window object's alert function and passes in the parameter 'button pressed!'
+				window.alert("Button Pressed and the Javascript worked!"); 
 			}
 
 // this is a Javascript comment
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-<h1> This is a website that I created to showcase my portfolio of projects that I have been working on. </h1>
+<h1> This is a website that I created to showcase my portfolio 
+of projects that I have been working on. </h1>
 
-<h2>Current Showcase List:</h2>
+<h2>Showcase List:</h2>
 <ul>
   <li>Index/Introduction</li>
   <li>Dark Willow Games</li>
